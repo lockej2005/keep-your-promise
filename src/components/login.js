00057e004f52bf85b8d3fe9950123ca0965a8d69
lockej2005 <josh@intelligentinstallations.com.au>
2023-08-01 00:20:43 +1000

    test endpoints

--- src/components/login.js
+++ src/components/login.js
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/login', {
+      const response = await axios.post('/api/login', {
         username,
         password
       }, { withCredentials: true });
